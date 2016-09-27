ba177fb8f2112833ec079992e8772069353470b7
Andrew <andrewsylcostello@gmail.com>
2016-09-27 22:12:19 +0100

    first

---
+++ character.py
@@ -0,0 +1,44 @@
+from monster import *
+from combat import Combat
+
+
+class Character(Combat):
+    attack_limit = 10
+    experience = 0
+    base_hit_points = 10
+
+    def attack(self):
+        roll = random.randint(1, self.attack_limit)
+        if self.weapon == 'sword':
+            roll = + 1
+        elif self.weapon == 'axe':
+            roll = + 2
+        return roll > 4
+
+    def weapon_choice(self):
+        weapon = raw_input('Weapon ([S]word, [A]xe, [B]ow, [St]taff): ').lower()
+        if weapon == 's':
+            return 'Sword'
+        elif weapon == 'a':
+            return 'Axe'
+        elif weapon == 'b':
+            return 'Bow'
+        elif weapon == 'St':
+            return 'Staff'
+        else:
+            self.weapon_choice()
+
+    def __init__(self, **kwargs):
+        self.name = raw_input('Name: ')
+        self.weapon = self.weapon_choice()
+        self.hit_points = self.base_hit_points
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def rest(self):
+        if self.hit_points < self.base_hit_points:
+            self.hit_points = + 1
+
+    def hit(self):
+        self.hit_points =- 1
+        return self.hit_points
---
+++ monster.py
@@ -0,0 +1,41 @@
+import random
+from combat import Combat
+
+COLOR = ('blue', 'green', 'black', 'orange')
+
+
+class Slime:
+    name = 'hat'
+    height = 50
+
+
+class Booger:
+    def __init__(self, **kwargs):
+        self.name = kwargs.get('name', 'steve')
+        self.height = kwargs.get('height', 700)
+
+
+class Monster(Combat):
+    def __init__(self, **kwargs):
+        self.name = kwargs.get('name', 'Rambo')
+        self.sound = kwargs.get('sound', 'Boo')
+        self.color = random.choice(COLOR)
+        self.hit_points = random.randint(1, 20)
+
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def battlecry(self):
+        return self.sound.upper()
+
+    def __str__(self):
+        return '%s %s : %s and %s and %s' % (self.name,
+                                             self.battlecry(),
+                                             self.__class__.__name__,
+                                             self.color,
+                                             self.hit_points
+                                             )
+
+
+class Goblin(Monster):
+    pass
